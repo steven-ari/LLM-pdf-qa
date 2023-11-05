09516611481c49e7494a0eefc6e2507da0263058
root <connectingsteven@gmail.com>
2023-11-05 13:29:48 +0000

    formattings

--- services/langchain/main.py
+++ services/langchain/main.py
@@ -1,19 +1,15 @@
 import requests
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 from bs4 import BeautifulSoup
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
 import streamlit as st
@@ -29,40 +25,47 @@ def extract_text_from(url):
     lines = (line.strip() for line in text.splitlines())
     return '\n'.join(line for line in lines if line)
 
+def process_text(text, db_dir):
+    # Split the text into chunks using langchain
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    
+    vectordb = Chroma.from_texts(
+        chunks,
+        embedding=OpenAIEmbeddings(),
+        persist_directory=db_dir
+    )
+    vectordb.persist()
+
+    return RetrievalQA.from_chain_type(
+        llm=OpenAI(),
+        retriever=vectordb.as_retriever(search_kwargs={'k': 6}),
+        return_source_documents=True
+    )
+
+def is_valid_url(text):
+    try:
+        result = urlparse(text)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+    
+
 def main():
     st.title("Chat with your PDF 💬")
     
-    pdf = st.file_uploader('Upload your PDF Document', type='pdf')
+    pdf = st.file_uploader('Upload your PDF Document, previous uploads will be saved', type='pdf')
     
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
-        # Text variable will store the pdf text
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+        text = "\n".join(page.extract_text() for page in pdf_reader.pages)
+        qa_chain = process_text(text, './data')
 
-        # Split the text into chunks using langchain
-        text_splitter = CharacterTextSplitter(
-            separator="\n",
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-        )
-        chunks = text_splitter.split_text(text)
-        
-        vectordb = Chroma.from_texts(
-            chunks,
-            embedding=OpenAIEmbeddings(),
-            persist_directory='./data'
-        )
-        vectordb.persist()
-
-        qa_chain = RetrievalQA.from_chain_type(
-            llm=OpenAI(),
-            retriever=vectordb.as_retriever(search_kwargs={'k': 6}),
-            return_source_documents=True
-        )
-        
         query = st.text_input('Ask your question just like in ChatGPT')
         cancel_button = st.button('Cancel')
         
@@ -76,39 +79,17 @@ def main():
                 
             st.write(result['result'])
     
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.title("OR chat with your Link 💬")
-    url = st.text_input("or enter a URL and chat with it")
+    for _ in range(7):
+        st.write("")
+
+
+    st.header('OR.. chat with your link 🔗 ', divider='rainbow')
+    # st.title("")
+    url = st.text_input("Enter a URL and chat with it, previous links will be saved")
     
-    if url:
+    if is_valid_url(url):
         text_url = extract_text_from(url)
-
-        # Split the text into chunks using langchain
-        text_splitter = CharacterTextSplitter(
-            separator="\n",
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-        )
-        chunks_url = text_splitter.split_text(text_url)
-        print(chunks_url)
-        
-        vectordb_url = Chroma.from_texts(
-            chunks_url,
-            embedding=OpenAIEmbeddings(),
-            persist_directory='./data'
-        )
-        vectordb_url.persist()
-
-        qa_chain_url = RetrievalQA.from_chain_type(
-            llm=OpenAI(),
-            retriever=vectordb_url.as_retriever(search_kwargs={'k': 6}),
-            return_source_documents=True
-        )
+        qa_chain_url = process_text(text_url, './data_url')
         
         query_url = st.text_input('Ask your question just like in ChatGPT to the html')
         cancel_button_url = st.button('Cancel url')
@@ -122,7 +103,10 @@ def main():
                 print(cost)
                 
             st.write(result_url['result'])
-            
+    else:
+        st.write('Give only valid url')
+    
+
             
 if __name__ == "__main__":
     main()
